274a33e62aa7efdb7ba725ff10c939844f316ac6
Anonta <SH-anonta@users.noreply.github.com>
2018-01-14 17:51:25 +0600

    Create Model factory classes for each model these class will be used in tests

---
+++ forum/unit_tests/modelFactory.py
@@ -0,0 +1,104 @@
+from forum.models import User, Board, Post, Reply
+
+# count of each model objects created
+#these are used to make sure two function calls don't create objects with the same values for unique fields
+user_cc= 0
+post_cc= 0
+board_cc= 0
+
+class UserFactory:
+    # This number is appended to unique field values
+    obj_count = 0
+
+    @classmethod
+    def nextid(cls):
+        cls.obj_count +=1
+        return cls.obj_count
+
+    @classmethod
+    def createUsers(cls,n):
+        uname= 'username'
+        pw= 'password'
+
+        users= []
+        for x in range(n):
+            name = uname + str(cls.nextid())
+            u = User.objects.create_user(username=name, password=pw)
+            users.append(u)
+
+        return users
+
+class BoardFactory:
+    # This number is appended to unique field values
+    obj_count = 0
+
+    @classmethod
+    def nextid(cls):
+        cls.obj_count +=1
+        return cls.obj_count
+
+    @classmethod
+    def createBoards(cls,n):
+        title = 'Board'
+
+        boards= []
+        for x in range(n):
+            t = title + str(cls.nextid())
+            b = Board.objects.create(title=t)
+            boards.append(b)
+
+        return boards
+
+class PostFactory:
+    # This number is appended to unique field values
+    obj_count = 0
+
+    @classmethod
+    def nextid(cls):
+        cls.obj_count +=1
+        return cls.obj_count
+
+    @classmethod
+    def createPosts(cls, n, user=None, board= None):
+        title = 'PostTitle'
+        content = 'PostContent'
+
+        if user is None:
+            user = UserFactory.createUsers(1)[0]
+        if board is None:
+            board = BoardFactory.createBoards(1)[0]
+
+        posts= []
+        for x in range(n):
+            t = title + str(cls.nextid())
+            p = Post.objects.create(title=t, content=content, board= board, creator=user)
+            posts.append(p)
+
+        return posts
+
+
+class ReplyFactory:
+    # This number is appended to unique field values
+    obj_count = 0
+
+    @classmethod
+    def nextid(cls):
+        cls.obj_count +=1
+        return cls.obj_count
+
+    @classmethod
+    def createReplies(cls, n, user=None, post=None):
+        content = 'ReplyContent'
+
+        if user is None:
+            user = UserFactory.createUsers(1)[0]
+
+        if post is None:
+            post = PostFactory.createPosts(1)[0]
+
+        replies= []
+        for x in range(n):
+            r = Reply.objects.create(content= content, reply_to=post, creator= user)
+            replies.append(r)
+
+        return replies
--- forum/unit_tests/test_models.py
+++ forum/unit_tests/test_models.py
@@ -3,19 +3,13 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from forum.models import User, Board, Post, Reply
+from forum.unit_tests import modelFactory
 
 
 class UserTest(TestCase):
     def test_creation(self):
-        uname = 'user'
-        pw= 'password'
-
-        u = User(username= uname)
-        u.set_password(pw)
-
-        u.save()
-
-        self.assertEqual(User.objects.count(), 1)
+        modelFactory.UserFactory.createUsers(2)
+        self.assertEqual(User.objects.count(), 2)
 
     def test_CustomUserModelIsInUse(self):
         self.assertEqual(User, get_user_model(), 'Customer User model is not in use')
@@ -24,32 +18,16 @@ class UserTest(TestCase):
 
 class BoardTest(TestCase):
     def test_creation(self):
-        Board.objects.create(title='Announcements')
-        Board.objects.create(title='General Discussion')
+        modelFactory.BoardFactory.createBoards(2)
         self.assertEqual(Board.objects.count(), 2)
 
 class PostTest(TestCase):
     def test_creation(self):
-        user= User.objects.create_user(username='User', password='password')
-        board= Board.objects.create(title='Announcements')
-
-        post = Post.objects.create(creator= user, title='Summer break', content= 'title', board= board)
-
-        self.assertEqual(Post.objects.count(), 1)
-
-        # check default values
-        self.assertEqual(post.deleted, False)
-        self.assertEqual(post.creator, user)
-        self.assertEqual(post.board, board)
+        modelFactory.PostFactory.createPosts(5)
+        self.assertEqual(Post.objects.count(), 5)
 
 class ReplyTest(TestCase):
     def test_creation(self):
-        user = User.objects.create_user(username='User', password='password')
-        board = Board.objects.create(title='Announcements')
-
-        post = Post.objects.create(creator=user, title='Summer break', content='title', board=board)
-
-        reply = Reply.objects.create(content = 'Good post,', reply_to= post, creator= user)
-
-        self.assertEqual(Reply.objects.count(), 1)
+        modelFactory.ReplyFactory.createReplies(3)
+        self.assertEqual(Reply.objects.count(), 3)
 

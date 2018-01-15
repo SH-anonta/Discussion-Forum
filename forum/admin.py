5be5f3163ef98eb8c43a4a43a9bc17d05d94cf60
Anonta <SH-anonta@users.noreply.github.com>
2018-01-16 02:46:09 +0600

    Fix avatar username not showing bug in post detail template registered UserProfile in site admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from forum.models import Board, Post, Reply
+from forum.models import Board, Post, Reply, UserProfile
 
 # registrations
 # admin.site.register(User, UserAdmin)
 admin.site.register(Board)
 admin.site.register(Post)
 admin.site.register(Reply)
+admin.site.register(UserProfile)

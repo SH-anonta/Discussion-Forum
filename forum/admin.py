71cc989f449eb06b3a69e31eb8a3eca5e7ed5ed8
Anonta <SH-anonta@users.noreply.github.com>
2018-01-16 01:51:46 +0600

    remove old commented out code

--- forum/admin.py
+++ forum/admin.py
@@ -1,39 +1,8 @@
 from django.contrib import admin
 from forum.models import Board, Post, Reply
 
-#todo delete
-#
-# class UserAdmin(BaseUserAdmin):
-#
-#     form = UserAdminChangeForm
-#     add_form = UserAdminCreationForm
-#
-#     list_display = ('username', 'admin')
-#     list_filter = ('admin',)
-#
-#     fieldsets = (
-#         (None, {'fields': ('username', 'password')}),
-#         ('Personal info', {'fields': ()}),
-#         ('Permissions', {'fields': ('admin',)}),
-#     )
-#
-#     add_fieldsets = (
-#         (None, {
-#             'classes': ('wide',),
-#             'fields': ('username', 'password1', 'password2')}
-#          ),
-#     )
-#
-#     search_fields = ('username',)
-#     ordering = ('username',)
-#     filter_horizontal = ()
-
-
 # registrations
 # admin.site.register(User, UserAdmin)
 admin.site.register(Board)
 admin.site.register(Post)
 admin.site.register(Reply)
-
-# unregistrations
-# admin.site.unregister(Group)
